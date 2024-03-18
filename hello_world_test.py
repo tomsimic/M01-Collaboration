d0aed49c2c273f3a0c95bec707d2305c812cccf8
Tomi Simic <tsimic@ivytech.edu>
2024-03-18 18:50:36 -0400

    Adding the hello_world_test.py file to the repo

---
+++ hello_world_test.py
@@ -0,0 +1,11 @@
+"""
+Program: hello_world_test.py
+Author: Tomi Simic
+Last date modified: 2024-03-18
+This program is to be used for M01 Lab.
+"""
+# pseudo code:
+# print Hello World on the terminal
+
+# Displaying print statement:
+print("Hello World")
