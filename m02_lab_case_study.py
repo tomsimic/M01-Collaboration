cebe852149dff0bb0c37ceb8eacbf336750025ad
Tomi Simic <tsimic@ivytech.edu>
2024-03-26 12:24:55 -0400

    M02 Lab - Case Study. Updated the program for continous loop until sentinel value is entered

--- m02_lab_case_study.py
+++ m02_lab_case_study.py
@@ -1,7 +1,7 @@
 """
 Program: m02_lab_case_study.py
 Author: Tomi Simic
-Last date modified: 2024-03-25
+Last date modified: 2024-03-26
 This program is to be used for Assignment 2 in Module 2. The program evaluates
 student's full name as only a alphabetical entry, student's GPA as a float,
 and outputs student's eligebility for Dean's List and Honor roll.
@@ -10,17 +10,24 @@ and outputs student's eligebility for Dean's List and Honor roll.
 # import needed function(s)
 # create a function that stores all prompts
 # create sentinel constant
-# prompt for student's last name and validate
-# prompt for student's first name and validate
-# prompt for GPA and store it as a float after validation
-# evaluate student GPA against Dean's List vs Honor Roll
+# create function to evaluate correct name entry
+# create function to evaluate correct gpa entry
+# create function to evaluate student GPA against Dean's List vs Honor Roll
+# create main function loop:
+#   prompt for student's last name and validate
+#   prompt for student's first name and validate
+#   prompt for GPA and store it as a float after validation
+#   display results
 
 # import re
 import re
 
+# Setting constants:
+SENTINEL = "ZZZ"
+QUIT = False
+
+
 # Prompts function:
-
-
 def prompts():
     """Provides prompt for use for user interaction"""
     prompts = {
@@ -43,12 +50,7 @@ acceptable range is between 0.1 and 4.0. ",
     return prompts
 
 
-# Setting constants:
-SENTINEL = "ZZZ"
-
 # User input validation:
-
-
 def name_validation(name_type):
     """This function checks for Sentinel value and user input"""
     entry = ""
@@ -80,12 +82,15 @@ def gpa():
         try:
             if isinstance(float(gpa), float):
                 gpa = float(gpa)
+                # Validating the value is within range:
                 if 0.1 < gpa <= 4.0:
-                    pass
+                    pass  # passing since the value is correct & returned later
                 else:
+                    # Resetting loop if entry is outside range
                     print(prompts().get('gpa_range') + prompts().get("retry"))
                     gpa = 0.0
         except ValueError:
+            # Validating a number is entered:
             if gpa.isnumeric() is False:
                 print(prompts().get('not_float') + prompts().get("retry"))
                 gpa = 0.0
@@ -95,22 +100,30 @@ def gpa():
 def student_type():
     """Checking student's grade standings"""
     gpa_average = gpa()
+    # Checking below honor's roll:
     if gpa_average < 3.25:
         return f"{prompts().get('average')} with a gpa average of {gpa_average}."
+    # Checking honor's roll:
     elif gpa_average < 3.5:
         return f"{prompts().get('honor')} with a gpa average of {gpa_average}."
+    # Returning dean's list:
     else:
         return f"{prompts().get('dean')} with a gpa average of {gpa_average}."
 
 
 # Main Function:
 if __name__ == "__main__":
-    last_name = name_validation('last_name')
-    if last_name == SENTINEL:
-        print(prompts().get('quit'))
-    else:
-        first_name = name_validation('first_name')
-        student_standings = student_type()
-        print("Student's first name: ", "%10s" % first_name)
-        print("Student's last name: ", "%12s" % last_name)
-        print("Student's GPA standings:", student_standings)
+    # Running main loop:
+    while QUIT is False:
+        last_name = name_validation('last_name')
+        # Checking for sentinel value to exit when needed:
+        if last_name == SENTINEL:
+            QUIT = True
+        # Executing main loop until sentinel is provided:
+        else:
+            first_name = name_validation('first_name')
+            student_standings = student_type()
+            print("Student's first name:    ", first_name)
+            print("Student's last name:     ", last_name)
+            print("Student's GPA standings: ", student_standings)
+    print(prompts().get('quit'))
