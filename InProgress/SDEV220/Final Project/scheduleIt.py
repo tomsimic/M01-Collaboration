d95efc42d185123c87e8d97a52e77e7d80b6e591
Tomi Simic <tsimic@ivytech.edu>
2024-04-16 14:42:01 -0400

    "Updating ignored list"

---
+++ InProgress/SDEV220/Final Project/scheduleIt.py
@@ -0,0 +1,40 @@
+"""
+Program: scheduleIt.py
+Author: Tomi Simic
+Last date modified: 2024-02-20
+This program is to be used as a module for the final project application for
+team project assignment.
+"""
+# pseudo code:
+# import needed modules
+# create class that stores the dates
+
+# Importing needed module(s):
+from dataclasses import dataclass
+from datetime import datetime as dd
+
+
+# Creating dataclass:
+@dataclass
+class ScheduleIt:
+    """Stores the schedule for the business"""
+    business_id: int
+    business_name: str
+    set_date = str
+    customer_name: str
+    # services: str
+
+    def addAppointment(self):
+        pass
+
+    def removeAppointment(self):
+        pass
+
+    def __repr__(self):
+        return f"{self.business_name}, {self.set_date} current date\
+and time: {dd.now()}"
+
+
+if __name__ == "__main__":
+    appointment = ScheduleIt(1, "WeInTheWoods", "today")
+    print(appointment)
---
+++ InProgress/SDEV220/zoo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"""
+Program: Zoo.py
+Author: Tomi Simic
+Last date modified: 2024-04-12
+This program is to be used for M04 Programming Assignment - Modules and Databases
+"""
+# pseudo code:
+# create a function to print a statement
+def hours():
+    print("Open 9-5")
+
+
+if name=="__main__":
+    hours()
+
